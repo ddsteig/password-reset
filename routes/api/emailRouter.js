5b09aac235cbfe27d3c47099a4e22155c18caa23
Danny <tekhexrax@yahoo.com>
2021-01-13 19:03:12 -0500

    Adding comments to code where needed

--- routes/api/emailRouter.js
+++ routes/api/emailRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const emailController = require("../../controllers/emailController");
-const userController = require("../../controllers/userController");
 
+// Matches with "/api/reset_password"
 router.route("/user/:email").post(emailController.sendPasswordResetEmail)
 
 module.exports = router;
